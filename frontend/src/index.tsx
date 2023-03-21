bca4f19a6b46eee54122e69a3922f3b471db49d0
Alex Hadley <alexhad6@gmail.com>
2023-03-21 12:01:30 -0700

    Begin setup with Vite

--- frontend/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode></StrictMode>
-);
