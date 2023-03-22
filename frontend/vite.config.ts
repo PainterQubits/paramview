835ce3c2054cd7c57553b08fd4fae6fcfac0537f
Alex Hadley <alexhad6@gmail.com>
2023-03-21 17:03:36 -0700

    Setup Prettier and ESLint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react()],
-})
+});
