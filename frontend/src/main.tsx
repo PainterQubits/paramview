46e405d23a300f0bd793fb0c065c18dab8b4a053
Alex Hadley <alexhad6@gmail.com>
2023-03-22 11:23:45 -0700

    Implement basic header

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "@/components/App";
+import { StrictMode } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { App } from "@/components";
+import theme from "@/theme";
+
+import "@fontsource/roboto/latin-400.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={theme()}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
