5cabe4ce0aafea41d3ccc93aebf7b8106410884f
Alex Hadley <alexhad6@gmail.com>
2023-03-30 13:08:12 -0700

    Update fake commit history

--- frontend/src/atoms/api.ts
+++ frontend/src/atoms/api.ts
@@ -2,10 +2,18 @@ import { atom } from "jotai";
 import { CommitEntry } from "@/types";
 
 /** Commit history retrieved from the server. */
-export const commitHistoryAtom = atom((): CommitEntry[] => {
-  return [
-    { id: 1, message: "Initial commit", timestamp: 1234567 },
-    { id: 2, message: "Second commit", timestamp: 12345678 },
-    { id: 3, message: "Third commit", timestamp: 123456789 },
-  ];
+export const commitHistoryAtom = atom(() => {
+  const now_timestamp = Date.now() / 1000;
+
+  const commitHistory: CommitEntry[] = [];
+
+  for (let i = 0; i < 20000; i++) {
+    commitHistory.push({
+      id: i + 1,
+      message: `Commit message`,
+      timestamp: now_timestamp - (999 - i) * 1000,
+    });
+  }
+
+  return commitHistory;
 });
