f870148d991bbc3035332de787835d80e3515ed9
Alex Hadley <alexhad6@gmail.com>
2023-03-31 16:57:16 -0700

    Update Flask server

--- paramview/_api.py
+++ paramview/_api.py
@@ -1,5 +1,11 @@
 """Flask blueprint for the backend API."""
 
-from flask import Blueprint
+from flask import Blueprint, current_app
 
 api = Blueprint("api", __name__, url_prefix="/api")
+
+
+@api.route("/database-name")
+def database_name() -> str:
+    """Return the commit history."""
+    return current_app.config["db_path"].stem
