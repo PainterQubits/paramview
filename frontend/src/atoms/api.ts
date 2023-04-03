5dbbdf2b0dcc9848bb32d196d4e657c9814c144d
Alex Hadley <alexhad6@gmail.com>
2023-04-03 13:29:17 -0700

    Start adding commit API to frontend

--- frontend/src/atoms/api.ts
+++ frontend/src/atoms/api.ts
@@ -1,19 +1,8 @@
 import { atom } from "jotai";
 import { CommitEntry } from "@/types";
+import { requestData } from "@/utils/api";
 
 /** Commit history retrieved from the server. */
 export const commitHistoryAtom = atom(() => {
-  const now_timestamp = Date.now() / 1000;
-
-  const commitHistory: CommitEntry[] = [];
-
-  for (let i = 0; i < 20000; i++) {
-    commitHistory.push({
-      id: i + 1,
-      message: `Commit message`,
-      timestamp: now_timestamp - (999 - i) * 1000,
-    });
-  }
-
-  return commitHistory;
+  return requestData<CommitEntry[]>("api/commit-history");
 });
