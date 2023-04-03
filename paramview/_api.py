760f06caafc27b7472ff9cf9ab0a6c4ca267fc45
Alex Hadley <alexhad6@gmail.com>
2023-04-03 12:08:42 -0700

    Add commit history api, remove astropy

--- paramview/_api.py
+++ paramview/_api.py
@@ -1,17 +1,33 @@
 """Flask blueprint for the backend API."""
 
+import os
+from typing import Any, cast
 from flask import Blueprint, current_app
-from paramview._db import DB
+from paramdb import ParamDB, CommitEntry
 
 api = Blueprint("api", __name__, url_prefix="/api")
 
 
-def get_db() -> DB:
-    """Get the database for the current app."""
-    return current_app.config["db"]
+class _CurrentDB:
+    """Wrapper to get properties from the database for the current app."""
+
+    def __getattribute__(self, name: str) -> Any:
+        db: ParamDB[Any] = current_app.config["db"]
+        return getattr(db, name)
+
+
+# Database for the current app
+_current_db = cast(ParamDB[Any], _CurrentDB())
 
 
 @api.route("/database-name")
-def database_name() -> str:
+def _database_name() -> str:
+    """Return the database name."""
+    current_db_path: str = current_app.config["db_path"]
+    return os.path.basename(current_db_path)
+
+
+@api.route("/commit-history")
+def _commit_history() -> list[CommitEntry]:
     """Return the commit history."""
-    return get_db().name
+    return _current_db.commit_history()
