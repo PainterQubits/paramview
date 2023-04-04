02c1a809f2996b4498a70a6c0e6bc054eaea7548
Alex Hadley <alexhad6@gmail.com>
2023-04-04 09:57:20 -0700

    Add port to arguments

--- paramview/_cli.py
+++ paramview/_cli.py
@@ -22,10 +22,17 @@ def _parse_args() -> Namespace:
         metavar="<database path>",
         help="path to the ParamDB database file",
     )
+    parser.add_argument(
+        "-p",
+        "--port",
+        default=5050,
+        type=int,
+        help="port to use (default is 5050)",
+    )
     return parser.parse_args()
 
 
 def main() -> None:
     """Parse command line arguments and start the server."""
     args = _parse_args()
-    start_server(args.db_path)
+    start_server(args.db_path, default_port=args.port)
