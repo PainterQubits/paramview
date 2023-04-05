38abac4e9bf99afdcad7c60ef52c1032b8fbbedc
Alex Hadley <alexhad6@gmail.com>
2023-04-05 12:21:36 -0700

    Add initial Typescript types for parameter data

--- frontend/src/atoms/api.ts
+++ frontend/src/atoms/api.ts
@@ -1,5 +1,5 @@
 import { atom } from "jotai";
-import { CommitEntry } from "@/types";
+import { CommitEntry, Data } from "@/types";
 import { requestData } from "@/utils/api";
 import { selectedCommitIndexAtom } from "@/atoms/commitSelect";
 
@@ -15,9 +15,9 @@ export const commitHistoryAtom = atom(() =>
   requestData<CommitEntry[]>("api/commit-history"),
 );
 
-/** Parameters for the currently selected commit. */
-export const paramsAtom = atom(async (get) => {
+/** Data for the currently selected commit. */
+export const dataAtom = atom(async (get) => {
   const commitHistory = await get(commitHistoryAtom);
   const selectedCommitIndex = await get(selectedCommitIndexAtom);
-  return requestData<object>(`api/params/${commitHistory[selectedCommitIndex].id}`);
+  return requestData<Data>(`api/data/${commitHistory[selectedCommitIndex].id}`);
 });
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as App } from "./App";
 export { default as Header } from "./Header";
-export { default as Params } from "./Params";
+export { default as ParamList } from "./ParamList";
