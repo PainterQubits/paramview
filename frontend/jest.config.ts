8391926260aedbedcd653362237f16e2bdcaa25d
Alex Hadley <alexhad6@gmail.com>
2023-04-17 14:28:05 -0700

    #11 Add yarn test to CI workflow

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,9 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
-  moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1",
-  },
+  moduleNameMapper: { "@/(.*)": "<rootDir>/src/$1" },
 };
 
 export default config;
