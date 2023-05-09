aadb2362bebb3df182c54c66b2f8553235bc10b0
Alex Hadley <alexhad6@gmail.com>
2023-05-08 17:47:35 -0700

    Make long commit history test more robust

--- frontend/cypress/e2e/parameter-navigation.cy.ts
+++ frontend/cypress/e2e/parameter-navigation.cy.ts
@@ -11,45 +11,43 @@ describe("parameter data for latest commit", () => {
 
   it("can display each type of parameter", () => {
     cy.getByTestId("parameter-list").within(() => {
-      cy.getByTestId("parameter-list-item-int").contains("123");
+      cy.getByTestId("parameter-list-item-int").should("contain", "123");
 
-      cy.getByTestId("parameter-list-item-float").contains("1.234"); // Rounded
+      cy.getByTestId("parameter-list-item-float").should("contain", "1.234"); // Rounded
 
-      cy.getByTestId("parameter-list-item-bool").contains("True");
+      cy.getByTestId("parameter-list-item-bool").should("contain", "True");
 
-      cy.getByTestId("parameter-list-item-str").contains("test");
+      cy.getByTestId("parameter-list-item-str").should("contain", "test");
 
-      cy.getByTestId("parameter-list-item-none").contains("none");
+      cy.getByTestId("parameter-list-item-none").should("contain", "none");
 
-      cy.getByTestId("parameter-list-item-date").containsDate(dateString);
+      cy.getByTestId("parameter-list-item-date").shouldContainDate(dateString);
 
-      cy.getByTestId("parameter-list-item-quantity").contains("1.234 m"); // Rounded
+      cy.getByTestId("parameter-list-item-quantity").should("contain", "1.234 m"); // Rounded
 
-      cy.getByTestId("parameter-list-item-list").as("list").contains("list");
-      cy.get("@list").containsDate(dateString).should("not.exist");
+      cy.getByTestId("parameter-list-item-list")
+        .should("contain", "list")
+        .shouldNotContainDate(dateString);
 
-      cy.getByTestId("parameter-list-item-dict").as("dict").contains("dict");
-      cy.get("@dict").containsDate(dateString).should("not.exist");
+      cy.getByTestId("parameter-list-item-dict")
+        .should("contain", "dict")
+        .shouldNotContainDate(dateString);
 
       cy.getByTestId("parameter-list-item-paramDict")
-        .as("paramDict")
-        .contains("ParamDict");
-      cy.get("@paramDict").containsDate(dateString).should("not.exist");
+        .should("contain", "ParamDict")
+        .shouldNotContainDate(dateString);
 
       cy.getByTestId("parameter-list-item-paramList")
-        .as("paramList")
-        .contains("ParamList");
-      cy.get("@paramList").containsDate(dateString).should("not.exist");
+        .should("contain", "ParamList")
+        .shouldNotContainDate(dateString);
 
       cy.getByTestId("parameter-list-item-struct")
-        .as("struct")
-        .contains("CustomStruct (Struct)");
-      cy.get("@struct").containsDate(dateString);
+        .should("contain", "CustomStruct (Struct)")
+        .shouldContainDate(dateString);
 
       cy.getByTestId("parameter-list-item-param")
-        .as("param")
-        .contains("CustomParam (Param)");
-      cy.get("@param").containsDate(dateString);
+        .should("contain", "CustomParam (Param)")
+        .shouldContainDate(dateString);
     });
   });
 
@@ -59,14 +57,16 @@ describe("parameter data for latest commit", () => {
       .find("input")
       .as("roundSwitchInput")
       .should("be.checked");
-    cy.getByTestId("parameter-list-item-float").as("float").contains("1.234");
-    cy.getByTestId("parameter-list-item-quantity").as("quantity").contains("1.234 m");
+    cy.getByTestId("parameter-list-item-float").as("float").should("contain", "1.234");
+    cy.getByTestId("parameter-list-item-quantity")
+      .as("quantity")
+      .should("contain", "1.234 m");
 
     // Unrounded
     cy.get("@roundSwitchInput").uncheck();
     cy.get("@roundSwitchInput").should("not.be.checked");
-    cy.get("@float").contains("1.2345");
-    cy.get("@quantity").contains("1.2345 m");
+    cy.get("@float").should("contain", "1.2345");
+    cy.get("@quantity").should("contain", "1.2345 m");
   });
 
   it("expands and collapses nested items when clicked", () => {
@@ -126,16 +126,12 @@ describe("parameter data for latest commit", () => {
     cy.getByTestId("collapse-all-button").click();
 
     // Children do not exist (collapsed)
-    cy.getByTestId("parameter-list-item-dict")
-      .as("dict")
-      .within(() => {
-        cy.getByTestId("parameter-list-item-int").should("not.exist");
-      });
-    cy.getByTestId("parameter-list-item-paramDict")
-      .as("paramDict")
-      .within(() => {
-        cy.getByTestId("parameter-list-item-int").should("not.exist");
-      });
+    cy.get("@dict").within(() => {
+      cy.getByTestId("parameter-list-item-int").should("not.exist");
+    });
+    cy.get("@paramDict").within(() => {
+      cy.getByTestId("parameter-list-item-int").should("not.exist");
+    });
   });
 
   it("resets root item to expanded when collapse all button is clicked", () => {
@@ -150,20 +146,16 @@ describe("parameter data for latest commit", () => {
     cy.get("@root").find('[role="button"]').first().click();
 
     // Children do not exist (collapsed)
-    cy.getByTestId("parameter-list-item-root")
-      .as("root")
-      .within(() => {
-        cy.getByTestId("parameter-list-item-int").should("not.exist");
-      });
+    cy.get("@root").within(() => {
+      cy.getByTestId("parameter-list-item-int").should("not.exist");
+    });
 
     // Click collapse all button
     cy.getByTestId("collapse-all-button").click();
 
     // Children are visible (expended)
-    cy.getByTestId("parameter-list-item-root")
-      .as("root")
-      .within(() => {
-        cy.getByTestId("parameter-list-item-int").should("be.visible");
-      });
+    cy.get("@root").within(() => {
+      cy.getByTestId("parameter-list-item-int").should("be.visible");
+    });
   });
 });
